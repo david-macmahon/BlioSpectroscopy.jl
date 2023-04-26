2d0bb98d05c4093a68e141a84e4d84a5fd424bcb
David MacMahon <davidm@astro.berkeley.edu>
2023-04-26 09:49:11 -0700

    Add Preferences support to BlioSpectroscopy

--- src/BlioSpectroscopy.jl
+++ src/BlioSpectroscopy.jl
@@ -1,2 +1,18 @@
 module BlioSpectroscopy
+    using Preferences
+
+    export getrawroot, geth5root, getcudadev, getsitedesc
+    export setrawroot, seth5root, setcudadev, setsitedesc
+
+    getrawroot(default::AbstractString=".") = @load_preference("RAWROOT", default)
+    setrawroot(dir::AbstractString) = @set_preferences!("RAWROOT"=>dir)
+
+    geth5root(default::AbstractString=".") = @load_preference("H5ROOT", default)
+    seth5root(dir::AbstractString) = @set_preferences!("H5ROOT"=>dir)
+
+    getcudadev() = @load_preference("CUDADEV", 0)
+    setcudadev(devnum::Integer) = @set_preferences!("CUDADEV"=>devnum)
+
+    getsitedesc() = @load_preference("SITEDESC", "unknown site")
+    setsitedesc(s::AbstractString) = @set_preferences!("SITEDESC"=>s)
 end # module BlioSpectroscopy
