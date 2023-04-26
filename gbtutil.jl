e4df9646c59e3c18e20436c0b3981a5b4960ebf3
David MacMahon <davidm@astro.berkeley.edu>
2023-04-26 10:22:24 -0700

    Tighten up some HDF5 usage

--- gbtutil.jl
+++ gbtutil.jl
@@ -1,12 +1,21 @@
 using HDF5, H5Zbitshuffle
 using Plots
+using Glob
 
 function hireswf(scan, fchan, nfchan=128; h5dir="/datax/dibas/AGBT22B_999_42/GUPPI")
     scanz = lpad(scan, 4, '0')
-    h5=h5open(glob("BLP??/*_$(scanz).rawspec.0000.h5", h5dir)[1])
+    h5names = glob("BLP??/*_$(scanz).rawspec.0000.h5", h5dir)
+    if isempty(h5names)
+        @warn "scan $scanz not found in $h5dir"
+        return nothing
+    end
+
     r = range(-nfchan÷2, length=nfchan)
-    h5d = h5["data"][r.+fchan,1,:]
-    foff = abs(h5["data"]["foff"][]) * 1e6
+
+    h5d, foff = h5open(h5names[1]) do h5
+        h5["data"][r.+fchan,1,:], abs(h5["data"]["foff"][]) * 1e6
+    end
+
     heatmap(r.*foff, 1:size(h5d,2), log10.(h5d'), yflip=true,
         title="Scan $scanz centered at fine channel $fchan", titlefontsize=12,
         xlabel="Frequency offset [Hz]", ylabel="Time sample"
